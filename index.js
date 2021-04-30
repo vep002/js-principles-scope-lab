a8efb3da59fabc3dea15d0c8cb617ffff6e5da75
Victoria <vep002@gmail.com>
2021-04-30 18:39:54 -0500

    commit tree 77e7c9473319df5771add9e2dd518268f94bdb28

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName= 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+ 
+}
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob'
+    return bestCustomer
+
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'sally';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'not sally'
+}
